2737d134b393ca9fa37783858279c779819d3d89
tommy <tomtrezb2003@gmail.com>
2017-10-30 17:27:32 -0500

    Removed reference to firebase for now

--- src/components/event-item.js
+++ src/components/event-item.js
@@ -3,10 +3,12 @@ import React from 'react';
 const EventItem = (props) => (
     <div className="event-item">
         <div className="event-item__image">
-            <img alt={props.title} src={props.imageURL} />
+            <img alt={props.title} src={props.event.imageURL} />
         </div>
         <div className="event-item__info">
-            <div className="event-item__title">{props.title}</div>
+            <div className="event-item__title">{props.event.title}</div>
+            <div className="event-item__into-item">{props.event.date}</div>
+            <div className="event-item__into-item">{props.event.format}</div>
         </div>
     </div>
 );
--- src/components/events-page.js
+++ src/components/events-page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import EventItem from './event-item';
 
-const EventsPage = (prop) => (
-    <div className="content-container">
-        <EventItem title="Halloween Spooktacular" />
-    </div>
-);
+const EventsPage = (prop) => {
+    const event = { title: 'Halloween Spooktacular', imageURL: '/images/banner.jpg', date: '10/28/2017', format: 'Modified Single Elimination'};
+    return (
+        <div className="content-container">
+            <EventItem event={event} />
+        </div>
+    );
+};
 
 export default EventsPage;
