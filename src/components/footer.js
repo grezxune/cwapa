1ee0bbb6fba2de436b719463d3532ed97f297b61
tommy <tomtrezb2003@gmail.com>
2017-11-28 11:28:59 -0600

    Social links target set to blank

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,9 @@ const Footer = (props) => (
     <div className="footer">
         <span>Central Wisconsin APA</span>
         <div className="footer__social-links">
-            <a href="https://facebook.com/centralwisconsinapa"><i className="fa fa-facebook"/></a>
-            <a href="tel:715-323-1554"><i className="fa fa-phone"/></a>
-            <a href="mailto:jdiedrich@apaleagues.com"><i className="fa fa-envelope"/></a>
+            <a href="https://facebook.com/centralwisconsinapa" target="_blank"><i className="fa fa-facebook"/></a>
+            <a href="tel:715-323-1554" target="_blank"><i className="fa fa-phone"/></a>
+            <a href="mailto:jdiedrich@apaleagues.com" target="_blank"><i className="fa fa-envelope"/></a>
         </div>
     </div>
 );
