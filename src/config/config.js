5f0cc9bb7287dc7220fd9cd05b8b9511baa72c15
tommy <tomtrezb2003@gmail.com>
2017-12-20 15:06:45 -0600

    Testing node_env value

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log('NODE_ENV: ', process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'development') {
     process.env.CW_API_URL = "http://localhost:3000";
