0c372f593fd815ecfeabf8bafd0e922300d9d8b0
tommy <tomtrezb2003@gmail.com>
2017-12-20 16:04:16 -0600

    Testing axios

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log('Custom Axios - CW_API_URL: ', process.env.CW_API_URL);
+
 const customAxios = axios.create({
     baseURL: process.env.CW_API_URL,
     withCredentials: true
