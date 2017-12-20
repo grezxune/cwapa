91c9d1e7654aac05310e08d42324d72ecc04648e
tommy <tomtrezb2003@gmail.com>
2017-12-20 15:02:50 -0600

    Update configs to use node_env

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,7 @@
-process.env.CW_API_URL = "https://cw-apa-api.herokuapp.com";
-process.env.CW_API_URL = "http://localhost:3000";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'development') {
+    process.env.CW_API_URL = "http://localhost:3000";
+} else {
+    process.env.CW_API_URL = "https://cw-apa-api.herokuapp.com";
+}
